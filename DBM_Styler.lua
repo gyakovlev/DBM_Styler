af12557a55698f7a26b3d9c9191dee561a16d93d
affli <delycid@gmail.com>
2011-01-21 20:05:55 +0300

    Styles per character and setup style on first launch.

--- DBM_Styler.lua
+++ DBM_Styler.lua
@@ -5,3 +5,15 @@ ns.dummy=function()end
 
 --
 ns.styles={}
+
+-- options frame
+local frame = CreateFrame("Frame", "DBM_Styler")
+frame.name = "DBM Styler"
+InterfaceOptions_AddCategory(frame)
+
+-- set default style on first launch
+if not DBM_StylerDB then
+	DBM_StylerDB={
+	["style"] = "thin",
+	}
+end
